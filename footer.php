7bc83c92cef41283321e56e8576cca5360f1f1e8
tqmm <397114202@qq.com>
2016-01-18 11:15:54 +0800

    fw

--- footer.php
+++ footer.php
@@ -4,13 +4,13 @@
     <div class="footer-left">
         <ul>
             <li><a href="index.html"><img src="images/home/logo.png" alt="遇见文化传媒" title="年终总结" height="53"/></a><span> |</span></li>
-            <li><p>Copyright &copy; 2012-2016. <span style="color: #F4A718;">the yujian culture media co.,ltd</span>. AllRights reserved. </p></li>
+            <li><p>Copyright &copy; 2012-2016. <a href="http://www.yujianchuanmei.cn"><span style="color: #F4A718;">苏州遇见文化传媒有限公司</span></a>. AllRights reserved. </p></li>
         </ul>
     </div>
     <div class="footer-right">
         <ul class="list-unstyled list-inline">
 
-            <li><span style="color: white;">苏州遇见文化传媒有限公司旗下项目：&nbsp;&nbsp;&nbsp;&nbsp;</span><a href="http://www.tecclub.cn"><img src="images/home/tec link.png"></a></li>
+            <li><a href="http://www.yujianchuanmei.cn"><span style="color: white;">苏州遇见文化传媒有限公司旗下项目：&nbsp;&nbsp;&nbsp;&nbsp;</span></a><a href="http://www.tecclub.cn"><img src="images/home/tec link.png"></a></li>
         </ul>
 
     </div>
