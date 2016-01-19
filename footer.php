c369137644c6f13ffb6fa7755547ce070bfe4a05
tqmm <397114202@qq.com>
2016-01-19 17:08:12 +0800

    fw

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <div class="footer">
     <div class="footer-left">
         <ul>
-            <li><a href="index.html"><img src="images/home/logo.png" alt="遇见文化传媒" title="年终总结" height="53"/></a><span> |</span></li>
+            <li><a href="/"><img src="images/home/logo.png" alt="遇见文化传媒" title="年终总结" height="53"/></a><span> |</span></li>
             <li><p>Copyright &copy; 2012-2016. <a href="http://www.yujianchuanmei.cn"><span style="color: #F4A718;">苏州遇见文化传媒有限公司</span></a>. AllRights reserved. </p></li>
         </ul>
     </div>
--- header.php
+++ header.php
@@ -13,10 +13,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="keywords" content="文化传媒 广告公司 活动公司 活动策划 活动执行" />
     <meta name="description" content="遇见传媒，致力于为客户提供专业活动策划执行全方位服务。成功服务于房产、汽车、银行、服装、IT、通讯、时尚品牌等多个行业的品牌企业，服务范围遍布全国。" />
-
-
     <!-- start-smoth-scrolling -->
-
 </head>
 
 <body>
@@ -25,22 +22,20 @@
         <!-- header -->
         <div class="header">
             <div class="logo">
-                <a href="index.php" title="遇见文化传媒"> <img class="img-responsive center-block" src="images/home/logo.png" alt="遇见文化logo" title="遇见文化传媒"/></a>
+                <a href="/" title="遇见文化传媒"> <img class="img-responsive center-block" src="images/home/logo.png" alt="苏州遇见文化传媒" title="苏州遇见文化传媒"/></a>
             </div>
             <div class="logo-right">
                 <span class="menu">
                    <button type="button" class="navbar-toggle navbar-default icontop" data-toggle="collapse" data-target=".navbar-main-collapse">
-
                        <i style="font-size: 20px;" class="glyphicon glyphicon-th-list"></i>
-
                    </button>
                 </span>
                 <ul class="nav1">
-                    <li <?php if($_SERVER['PHP_SELF']=='/index.php'){echo ' class="cap" ';}?> title="首页"><a href="index.php" class="act hidden-xs hidden-sm">HOME</a><a href="index.php" class="act visible-sm visible-xs">遇见首页</a></li>
-                    <li <?php if($_SERVER['PHP_SELF']=='/about.php'){echo ' class="cap" ';}?> title="我们是谁？"><a href="about.php" class="act1 hidden-xs hidden-sm">ABOUT US</a><a href="about.php" class="act1 visible-sm visible-xs">关于遇见</a></li>
-                    <li <?php if($_SERVER['PHP_SELF']=='/services.php'){echo ' class="cap" ';}?> title="我们能做什么？"><a href="services.php" class="act2 hidden-xs hidden-sm">SERVICES</a><a href="services.php" class="act2 visible-sm visible-xs">遇见服务</a></li>
-                    <li <?php if($_SERVER['PHP_SELF']=='/case.php'){echo ' class="cap" ';}?> title="我们做过什么"><a href="case.php" class="act3 hidden-xs hidden-sm">CASE</a><a href="case.php" class="act3 visible-sm visible-xs">遇见案例</a></li>
-                    <li <?php if($_SERVER['PHP_SELF']=='/contact.php'){echo ' class="cap" ';}?> title="联系我们"><a href="contact.php" class="act5 hidden-xs hidden-sm">CONTACT US</a><a href="contact.php" class="act5 visible-sm visible-xs">联系遇见</a></li>
+                    <li <?php if($_SERVER['PHP_SELF']=='/index.php'){echo ' class="cap" ';}?> title="遇见首页"><a href="index.php" class="act hidden-xs hidden-sm">HOME</a><a href="index.php" class="act visible-sm visible-xs">遇见首页</a></li>
+                    <li <?php if($_SERVER['PHP_SELF']=='/about.php'){echo ' class="cap" ';}?> title="遇见文化是谁"><a href="about.php" class="act1 hidden-xs hidden-sm">ABOUT US</a><a href="about.php" class="act1 visible-sm visible-xs">关于遇见</a></li>
+                    <li <?php if($_SERVER['PHP_SELF']=='/services.php'){echo ' class="cap" ';}?> title="遇见能做什么"><a href="services.php" class="act2 hidden-xs hidden-sm">SERVICES</a><a href="services.php" class="act2 visible-sm visible-xs">遇见服务</a></li>
+                    <li <?php if($_SERVER['PHP_SELF']=='/case.php'){echo ' class="cap" ';}?> title="遇见做过什么"><a href="case.php" class="act3 hidden-xs hidden-sm">CASE</a><a href="case.php" class="act3 visible-sm visible-xs">遇见案例</a></li>
+                    <li <?php if($_SERVER['PHP_SELF']=='/contact.php'){echo ' class="cap" ';}?> title="联系遇见"><a href="contact.php" class="act5 hidden-xs hidden-sm">CONTACT US</a><a href="contact.php" class="act5 visible-sm visible-xs">联系遇见</a></li>
                 </ul>
             </div>
             <div class="clearfix"> </div>
