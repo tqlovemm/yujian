7671ca8b0140ef6246de71cbf275ddb7374a1c52
tqmm <397114202@qq.com>
2016-02-25 10:10:21 +0800

    fw

--- footer.php
+++ footer.php
@@ -9,15 +9,20 @@
     </div>
     <div class="footer-right">
         <ul class="list-unstyled list-inline">
-
             <li><a href="http://www.yujianchuanmei.cn"><span style="color: white;">苏州遇见文化传媒有限公司旗下项目：&nbsp;&nbsp;&nbsp;</span></a><a href="http://www.tecclub.cn"><img class="tec-logo" alt="TEC引擎俱乐部" title="点击进入TEC引擎俱乐部" src="images/home/tec link.png"></a></li>
         </ul>
-
     </div>
     <div class="clearfix"> </div>
+
+    <div class="row">
+        <ul class="list-unstyled list-inline text-center friendly">
+            <li><span style="color:white;">友情链接：</span><a href="http://www.ffsjgzs.com/"><span style="color: white;">非凡视觉</span></a></li>
+        </ul>
+    </div>
+
 </div>
 <!-- //footer -->
-</div>
+
 </div>
 <div class="hidden">
     <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1257371344'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1257371344%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
