95f36290c5e38601799a17669c36d19007f34934
tqmm <397114202@qq.com>
2016-03-17 11:48:42 +0800

    fw

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <div class="clearfix"></div>
 
 <div class="footer">
-    <div class="footer-left">
-        <ul>
+    <div class="footer-left pull-left">
+        <ul class="list-unstyled">
             <li><a href="/"><img src="images/home/yujian-logo.png" alt="遇见文化传媒" title="年终总结" height="53"/></a><span> |</span></li>
             <li><p>Copyright &copy; 2012-2016. <a href="http://www.yujianchuanmei.cn"><span style="color: #F4A718;">苏州遇见文化传媒有限公司</span></a>. AllRights reserved. </p></li>
         </ul>
     </div>
-    <div class="footer-right">
+    <div class="footer-right pull-right">
         <ul class="list-unstyled list-inline">
             <li><a href="http://www.yujianchuanmei.cn"><span style="color: white;">苏州遇见文化传媒有限公司旗下项目：&nbsp;&nbsp;&nbsp;</span></a><a href="http://www.tecclub.cn"><img class="tec-logo" alt="TEC引擎俱乐部" title="点击进入TEC引擎俱乐部" src="images/home/tec link.png"></a></li>
         </ul>
@@ -25,6 +25,16 @@
 
 </div>
 <div class="hidden">
+    <script>
+        var _hmt = _hmt || [];
+        (function() {
+            var hm = document.createElement("script");
+            hm.src = "//hm.baidu.com/hm.js?b315857d2517b788eb87c59fbf56ba99";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    </script>
+
     <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1257371344'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1257371344%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
 </div>
 </body>
